123338e1e47248c2301962ade401a53c006cd53a
Winton Wang <winton_wang@yahoo.com>
2020-08-10 23:47:07 -0400

    add AuthRepository

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -7,12 +7,15 @@ namespace DatingApp.API.Data
 {
     public class DataContext : DbContext
     {
-
         public DataContext(DbContextOptions<DataContext> options) : base(options){
 
         }
 
         public DbSet<Value> Values {get; set;}
+
+        public DbSet<User> Users {get; set;}
+
+
 
     }
 
